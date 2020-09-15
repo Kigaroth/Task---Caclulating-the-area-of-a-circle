266e84618388a895b7759bfe8abdd7a6999246e3
Kigaroth <hp_20@hotmail.com>
2020-09-15 16:17:43 +0200

    Fixed with

--- Task - Caclulating the area of a circle/Task - Caclulating the area of a circle.cpp
+++ Task - Caclulating the area of a circle/Task - Caclulating the area of a circle.cpp
@@ -4,23 +4,29 @@
 #include <iostream>
 #include <vector>
 
-double radius = 0;
-double height = 0;
+double circle_radius = 0;
+double cylinder_height = 0;
 const double pi = 3.14;
 
-double area(double radius);
-double volume(double radius, double height);
+double area(double);
+double volume(double, double);
 
 int main()
 {
-    std::cout << "Enter the radius of the circle: ";
-    std::cin >> radius;
+    std::cout << " Enter the radius of the circle: ";
+    std::cin >> circle_radius;
     
-    std::cout << "Thank you.\n";
+    std::cout << " Thank you.\n";
 
-    std::cout << "Enter the height of the circle: ";
-    std::cin >> height;
+    std::cout << " Enter the height of the circle: ";
+    std::cin >> cylinder_height;
+    
+    std::cout << std::endl;
+    
+    std::cout << " The area of the circle is: " << area(circle_radius) << " m^2" << std::endl;
+    std::cout << " The volume of the circle is: " << volume(circle_radius, cylinder_height) <<" m^3" << std::endl;
 
+    return 0;
 }
 
 double area(double radius) {
